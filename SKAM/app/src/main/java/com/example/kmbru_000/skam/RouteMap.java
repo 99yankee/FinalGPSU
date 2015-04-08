5b81879c3bd3df297c66b6047e74297f873aaf47
Alec <awecheni@syr.edu>
2015-04-07 20:33:06 -0400

    Buses
    add initial bus function

---
+++ SKAM/app/src/main/java/com/example/kmbru_000/skam/RouteMap.java
@@ -0,0 +1,15 @@
+package com.example.kmbru_000.skam;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class RouteMap extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_map);
+    }
+
+}
